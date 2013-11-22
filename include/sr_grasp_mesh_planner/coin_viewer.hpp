72be25a88088db1f13feb8f2f7cb6e859a3d7e16
Yi Li <yi@shadowrobot.com>
2013-11-22 17:41:01 +0000

    Remove boost::unique_lock from class CoinViewer.

--- include/sr_grasp_mesh_planner/coin_viewer.hpp
+++ include/sr_grasp_mesh_planner/coin_viewer.hpp
@@ -1,27 +1,23 @@
- #pragma once
+#pragma once
 
- #include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
- #include <Inventor/threads/SbMutex.h>
- #include <boost/thread.hpp>
+#include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
+#include <Inventor/threads/SbMutex.h>
 
- class CoinViewer : public SoQtExaminerViewer
- {
- public:
+class CoinViewer : public SoQtExaminerViewer
+{
+public:
+  CoinViewer(QWidget *parent=NULL,
+             const char *name=NULL,
+             SbBool embed=TRUE,
+             SoQtFullViewer::BuildFlag flag=BUILD_ALL,
+             SoQtViewer::Type type=BROWSER);
+  virtual ~CoinViewer();
 
-     CoinViewer(QWidget *parent=NULL,
-                const char *name=NULL,
-                SbBool embed=TRUE,
-                SoQtFullViewer::BuildFlag flag=BUILD_ALL,
-                SoQtViewer::Type type=BROWSER);
-     virtual ~CoinViewer();
+  void lock();
+  void unlock();
 
-     void lock();
-     void unlock();
+protected:
+  virtual void actualRedraw(void);
 
- protected:
-     virtual void actualRedraw(void);
-
-     SbThreadMutex mutex;
-     //mutable boost::recursive_mutex mutex;
-     //boost::unique_lock<boost::recursive_mutex> _lock;
- };
+  SbThreadMutex sbmutex_;
+};
--- src/coin_viewer.cpp
+++ src/coin_viewer.cpp
@@ -1,35 +1,45 @@
 #include "sr_grasp_mesh_planner/coin_viewer.hpp"
 
+//-------------------------------------------------------------------------------
+
 CoinViewer::CoinViewer(QWidget *parent,
                        const char *name,
                        SbBool embed,
                        SoQtFullViewer::BuildFlag flag,
                        SoQtViewer::Type type)
-: /*_lock(mutex, boost::defer_lock),*/ SoQtExaminerViewer(parent, name, embed, flag, type)
+: SoQtExaminerViewer(parent, name, embed, flag, type)
 {
 }
+
+//-------------------------------------------------------------------------------
 
 CoinViewer::~CoinViewer()
 {
 }
 
+//-------------------------------------------------------------------------------
+
 void CoinViewer::lock()
 {
-  //_lock.lock();
-  mutex.lock();
+  sbmutex_.lock();
 }
+
+//-------------------------------------------------------------------------------
 
 void CoinViewer::unlock()
 {
-  //_lock.unlock();
-  mutex.unlock();
+  sbmutex_.unlock();
 }
+
+//-------------------------------------------------------------------------------
 
 void CoinViewer::actualRedraw()
 {
-  //_lock.lock();
-  mutex.lock();
+  sbmutex_.lock();
+
   SoQtExaminerViewer::actualRedraw();
-  //_lock.unlock();
-  mutex.unlock();
+
+  sbmutex_.unlock();
 }
+
+//-------------------------------------------------------------------------------
